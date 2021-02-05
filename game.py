63a5b2c3516b7cdfcea2eac5d77ca8fd89848092
Uspinhal <uspinhal@yahoo.com>
2021-02-05 15:27:29 -0300

    Estruturando o projeto

--- game.py
+++ game.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from models.calcular import Calcular
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    pontos: int = 0
+    jogar(pontos)
+    
+    
+def jogar(pontos: int) -> None:
+    pass
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
